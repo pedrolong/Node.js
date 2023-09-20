2b7125791d114a8a7ada5e55a851cc2f33f13e46
Pedro Henrique Longo <125682292+pedrolong@users.noreply.github.com>
2023-09-20 13:14:37 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,8 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('20/09 - Pedro Henrique Longo');
-}).listen(8021); 
+var dataAtual = new Date() ;
+var dataFormatada =  dataAtual.toDateString () ; 
+  res.end('20/09 - Pedro Henrique longo\nData atual: ' + dataFormatada);
+}).listen(8021);
+
